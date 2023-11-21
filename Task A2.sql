49f5ba39be4e2904b34f56fc3182fedab13b77f4
RQ92 <150052750+RQ92@users.noreply.github.com>
2023-11-21 15:22:35 +0000

    Update Task A2.sql

--- Task A2.sql
+++ Task A2.sql
@@ -4,23 +4,28 @@ from Seattle_cycles_station
 select count(distinct(station_id)) as "DistinctIDs" -- counts the number of unique station IDs (all are unique)
 from Seattle_cycles_station
 
+
 select name, decommission_date
 from Seattle_cycles_station
 where decommission_date is not null -- this shows the stations which have been decomission
 
+--4)
 select count(*) as "NoOfDecomStats" -- this counts the number of stations which have a decommisioned date
 from Seattle_cycles_station
 where decommission_date is not null
-
+--5)
 select min(install_date) as "earliest install date", max(install_date) as "latest install date"
 from Seattle_cycles_station -- we get the earliest and latest install date
 
+--6)
 select count(distinct(trip_id)) as "UniqueTrips", count(*) as "RowCount"
 from Seattle_cycles_trip -- returns unique trips and the total row count
 
-select tripduration / 60 as "TripDurationMins"
+--7)
+select AVG(tripduration / 60) as "TripDurationMins"
 from Seattle_cycles_trip -- converts the tripduration column to minutes
 
+--8)
 select count(distinct(trip_id)) as "TripCount", from_station_name -- counts the number of unique trips per station
 from Seattle_cycles_trip
 group by from_station_name -- this allows us to get the count for each station
@@ -35,6 +40,7 @@ select count(*) -- returns the number of rows which contain a birth year
 from Seattle_cycles_trip
 where birthyear is not null
 
+--11)
 select 2023 - max(birthyear) as "YoungestRider" -- this returns the current age of the youngest rider
 from Seattle_cycles_trip
 
@@ -54,5 +60,41 @@ where usertype = 'Short-Term Pass Holder'
 group by usertype
 
 --14)
-select
-from Seattle_weather_conditions
+select Min(date) as EarliestDate, Max(date) as LatestDate
+from Seattle_weather_daily
+
+--15)
+select date, Max_Temperature_F - Min_TemperatureF as TempDiff, Max_Temperature_F, Min_TemperatureF
+from Seattle_weather_daily -- returns the daily temp diff and orders by largest to smallest
+order by TempDiff desc
+
+--16)
+select avg(Mean_Humidity) as AvgHumidity, Month(date) as "Month", year(date) as "Year"
+from Seattle_weather_daily
+group by Month(date), Year(date) -- returns the average humidity of each month
+order by avg(Mean_Humidity) desc
+
+--17) 
+select max(Max_Wind_Speed_MPH) as MaxWindSpeed, month(date) as Month
+from Seattle_weather_daily
+where year(date) = 2015
+group by month(date) -- returns max wind speed of each month in 2015
+order by max(Max_Wind_Speed_MPH) desc
+
+--18) 
+select count(Events) -- returns how many days had a weather event other than just rain
+from Seattle_weather_daily
+where events is not null
+and events <> 'Rain'
+
+--19)
+select sum(Precipitation_In) as TotalRainfall, year(date) as year -- total rainfall in first 3 months of 2016
+from Seattle_weather_daily
+where month(date) In(1,2,3) and year(date)= 2016
+group by year(date) -- group by year so the sum is showed as one figure for all 3 months
+
+--20)
+select count(Events) as FogCount
+from Seattle_weather_daily
+where events like '%fog%'
+
